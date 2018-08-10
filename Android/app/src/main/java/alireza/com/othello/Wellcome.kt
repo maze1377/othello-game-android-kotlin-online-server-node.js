50b84d1e3819f0216891d964dffa4b4d172f55f9
Alireza Rahimi <ali938414@gmail.com>
2018-08-10 11:54:27 +0430

    buidling project(1)

--- Android/app/src/main/java/alireza/com/othello/Wellcome.kt
+++ Android/app/src/main/java/alireza/com/othello/Wellcome.kt
@@ -8,5 +8,7 @@ class Wellcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wellcome)
+
+
     }
 }
