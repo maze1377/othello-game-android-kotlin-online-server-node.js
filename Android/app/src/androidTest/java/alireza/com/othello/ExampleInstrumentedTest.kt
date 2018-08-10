405e933cdcbdf3c702418481cde7572b4bee11fc
Alireza Rahimi <ali938414@gmail.com>
2018-08-10 11:51:10 +0430

    buidling project

---
+++ Android/app/src/androidTest/java/alireza/com/othello/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package alireza.com.othello
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("alireza.com.othello", appContext.packageName)
+    }
+}
---
+++ Android/app/src/main/java/alireza/com/othello/Wellcome.kt
@@ -0,0 +1,12 @@
+package alireza.com.othello
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Wellcome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wellcome)
+    }
+}
